4f66edbadc4b407725abba1a42ade3e0591be63a
yahbouss <47297430+yahbouss@users.noreply.github.com>
2021-03-01 13:57:36 +0100

    exercise code

--- Raspberry Pi Part/main.py
+++ Raspberry Pi Part/main.py
@@ -1,31 +1,13 @@
-import psycopg2
-DATABASE_URL = "postgres://ddrwyxdrclgapj:2d618ce3846acfe8048a05384a5242e377ee04d576efde709256c18f3291a323@ec2-34-248-148-63.eu-west-1.compute.amazonaws.com:5432/d83pdd4puormrb"
-DB = psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+led = 10
+sensor = 8
+GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
 
-
-def collectFromDB(entredPin):
-    DB.execute("SELECT * FROM clients WHERE pin = " + str(entredPin) + ";")
-    collected = DB.fetchone()
-    if collected != "None":
-        output = {
-            "id": collected[8],
-            "name": collected[0],
-            "surname": collected[1],
-            "studentID": collected[2],
-            "DATE": collected[3],
-            "Time_IN": collected[4],
-            "Time_OUT": collected[5],
-            "pin": collected[6],
-            "email": collected[7]
-        }
-    else:
-        output = "none"
-    return output
-
-pin = input()
-
-if collectFromDB(int(pin)) != "none":
-    print(collectFromDB(pin)["name"])
-    print(True)
+if GPIO.input(sensor):
+    print('Input was HIGH')
+    GPIO.output(led, GPIO.HIGH)
 else:
-    print(False)
+    print('Input was LOW')
+    GPIO.output(led, GPIO.LOW)
