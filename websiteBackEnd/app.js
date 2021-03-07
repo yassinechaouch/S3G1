5585eea1641d9e1398ad5f265bc2e1f0486f0696
yahbouss <yahbouss@hotmail.fr>
2021-03-07 18:51:05 +0100

    return from db

--- websiteBackEnd/app.js
+++ websiteBackEnd/app.js
@@ -3,21 +3,19 @@ const app = express();
 const dotenv = require('dotenv').config()
 const {connect} = require('./models/DBconnect')
 const morganBody = require('morgan-body')
+const form = require('./routes/form')
 
 morganBody(app);
 
 connect()
-
-//whatever
-const form = require('./routes/form')
 
 const port = 5000;
 
 app.use(express.json())
 app.use('/', form)
 
-app.post('/', (req, res)=>{
-  res.send(req.body)
+app.get('/', (req, res)=>{
+  res.send('here')
 })
 
 app.listen(port, () => {
--- websiteBackEnd/routes/form.js
+++ websiteBackEnd/routes/form.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const form = require('../controllers/formValid')
+const dataGet = require('../controllers/dataGet')
 
 router.get('/form', (req,res)=> res.send('waiting for post'))
 
 router.post('/form', form)
+
+router.get('/data', dataGet)
 
 module.exports = router
